abaf2b1d68b7052722114f099dac386913972667
lolmisiooo <lolmisiooo@gmail.com>
2026-07-09 12:10:33 +0200

    Add manual price list in prices.lua

---
+++ prices.lua
@@ -0,0 +1,73 @@
+-- ========== MANUAL PRICE LIST ========== [""] = ,
+local PRICES = {
+  -- Toys
+    Toys = 20,
+  -- Clothing
+    Clothing = 30,
+    Shoes = 40,
+    Hats = 50,
+  -- Candy
+    Candy = 8,
+  -- Food
+    Snacks = 7,
+    ["Canned Food"] = 10,
+    ["Boxed Food"] = 12,
+    ["Cold Food"] = 15,
+    ["Baked Goods"] = 12,
+    Fruit = 10,
+    Vegetables = 10,
+    Condiments = 10,
+    Beverages = 8,
+  -- Electronics
+    ["Small Electronics"] = 400,
+    ["Large Electronics"] = 700,
+    ["PC Electronics"] = 450,
+    ["PC Peripherals"] = 85,
+    ["Office Electronics"] = 45,
+    ["Game Consoles"] = 290,
+    Videogames = 35,
+  -- Appliances
+    ["Small Appliances"] = 200,
+    ["Large Appliances"] = 1000,
+  -- Home Goods
+    Kitchenware = 40,
+    ["Paper Goods"] = 12,
+    Luggage = 70
+    ["Vacuum Cleaners"] = 100,
+    ["Personal Care"] = 18,
+  -- School Supplies
+    ["School Supplies"] = 15,
+  -- Instruments
+    ["Brass Instruments"] = 500,
+    ["String Instruments"] = 500,
+    Pianos = 650,
+  -- Tools and Equipment
+    ["Small Outdoor Equipment"] = 130,
+    ["Large Outdoor Equipment"] = 500,
+    ["Small Power Tools"] = 65,
+    ["Large Power Tools"] = 115,
+    ["Garden Tools"] = 40,
+    ["Paint Supplies"] = 18,
+    ["Lumber"] = 20,
+  -- Furniture
+    ["Small Furniture"] = 180,
+    ["Large Furniture"] = 450,
+    ["Exercise Equipment"] = 1200,
+    ["Game Tables"] = 155,
+  -- Transportation
+    ["Personal Transport"] = 425,
+  -- Vehicles
+    ["Car Parts"] = 50,
+    ["Economy Cars"] = 13000,
+  -- Jewelery
+    Jewelery = 850,
+  -- Pets
+    ["Small Pets"] = 65,
+    ["Large Pets"] = 600,
+    ["Pet Food"] = 20,
+  -- Military
+    Guns = 850,
+    Ammo = 70,
+    ["Military Vehicles"] = 90000,
+    ["Armored Vehicles"] = 485000,
+}
